05c6d65398257b0f022db2e82eef3343411a7bf0
Alexandre Cavalari <alexandre.cavalari@outlook.com>
2024-03-02 16:18:18 -0300

    Adicionado classe BibliotecaVirtual
    Adicionada a classe BibliotecaVirtual ao pacote livrousuario. Esta classe inclui métodos para adicionar livros e usuários ao sistema, listar livros disponíveis para aluguel, alugar e devolver livros. Cada livro possui um status de aluguel para rastrear se está disponível ou não.

--- biblioteca/BibliotecaVirtual.java
+++ biblioteca/BibliotecaVirtual.java
@@ -1,5 +1,58 @@
 package livrousuario;
 
-public class BibliotecaVirtual {
+import java.util.ArrayList;
+import java.util.List;
 
+public class BibliotecaVirtual {
+    // Listas para armazenar livros e usuários do sistema
+    private List<Livro> livros;
+    private List<Usuario> usuarios;
+
+    // Construtor da classe SistemaAluguel
+    public BibliotecaVirtual() {
+        // Inicializa as listas de livros e usuários
+        this.livros = new ArrayList<>();
+        this.usuarios = new ArrayList<>();
+    }
+
+    // Método para adicionar um livro ao sistema
+    public void adicionarLivro(Livro livro) {
+        livros.add(livro);
+    }
+
+    // Método para adicionar um usuário ao sistema
+    public void adicionarUsuario(Usuario usuario) {
+        usuarios.add(usuario);
+    }
+
+    // Método para listar os livros disponíveis para aluguel
+    public List<Livro> listarLivrosDisponiveis() {
+        List<Livro> livrosDisponiveis = new ArrayList<>();
+        for (Livro livro : livros) {
+            if (!livro.isAlugado()) {
+                livrosDisponiveis.add(livro);
+            }
+        }
+        return livrosDisponiveis;
+    }
+
+    // Método para alugar um livro para um usuário
+    public boolean alugarLivro(Livro livro, Usuario usuario) {
+        if (livros.contains(livro) && usuarios.contains(usuario) && !livro.isAlugado()) {
+            livro.setAlugado(true);
+            return true; // Aluguel Ok
+        } else {
+            return false; // Aluguel falhou
+        }
+    }
+
+    // Método para devolver um livro ao sistema
+    public boolean devolverLivro(Livro livro) {
+        if (livros.contains(livro) && livro.isAlugado()) {
+            livro.setAlugado(false);
+            return true; // Devolução Ok
+        } else {
+            return false; // Devolução falhou
+        }
+    }
 }
